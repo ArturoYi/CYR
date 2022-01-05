20fbd7834adaefa0146ef88f9de94ce15d3c41b3
540765 <3062995371@qq.com>
2022-01-05 14:29:20 +0800

    更新

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ set -e
 #验证你是否关联仓库
 # deploy to github
 msg=`date +%Y-%m-%d`
-end=10
+var1=10
 githubUrl=git@github.com:540765/CYR.git
 
 if [ -a "$GITHUB_TOKEN" ] 
@@ -30,14 +30,6 @@ fi
 if [ "$?"=0 ]
 then
   echo "无错误"
-else
-  git config --global user.name "540765"
-  git config --global user.email "3062995371@qq.com"
-  git add .
-  git commit -m "更新"
-  git push -u origin master
-fi
-var1=10
 while [ $var1 -gt 0 ]
 do 
    echo -ne $var1
@@ -45,6 +37,14 @@ do
    sleep 1
    echo -ne "\r   \r" #清空行
 done
+else
+  git config --global user.name "540765"
+  git config --global user.email "3062995371@qq.com"
+  git pull --rebase
+  git add .
+  git commit -m "更新"
+  git push -u origin master
+fi
 # git init
 # git add .
 # git commit -m "更新"
