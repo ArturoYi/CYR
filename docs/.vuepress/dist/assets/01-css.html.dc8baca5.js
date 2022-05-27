272bdd4f93e281063388593ce705ff29011c9668
TRANBIOT-PZ004\Yiren <yiren@tranbiot.com>
2022-05-27 18:55:33 +0800

    2022-05-27更新

---
+++ docs/.vuepress/dist/assets/01-css.html.dc8baca5.js
@@ -0,0 +1,32 @@
+import { o as openBlock, c as createElementBlock, F as Fragment, b as createBaseVNode, d as createTextVNode } from "./app.abab5812.js";
+import { _ as _export_sfc } from "./plugin-vue_export-helper.21dcd24c.js";
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createBaseVNode("h2", {
+  id: "\u4EC0\u4E48\u662Fcss",
+  tabindex: "-1"
+}, [
+  /* @__PURE__ */ createBaseVNode("a", {
+    class: "header-anchor",
+    href: "#\u4EC0\u4E48\u662Fcss",
+    "aria-hidden": "true"
+  }, "#"),
+  /* @__PURE__ */ createTextVNode(" \u4EC0\u4E48\u662FCSS\uFF1F")
+], -1);
+const _hoisted_2 = /* @__PURE__ */ createBaseVNode("p", null, [
+  /* @__PURE__ */ createBaseVNode("code", null, "CSS"),
+  /* @__PURE__ */ createTextVNode(" \u2014 \u662F\u4F60\u5728HTML\u4E4B\u540E\u5E94\u8BE5\u5B66\u4E60\u7684\u7B2C\u4E8C\u95E8\u6280\u672F\u3002"),
+  /* @__PURE__ */ createBaseVNode("code", null, "HTML"),
+  /* @__PURE__ */ createTextVNode("\u7528\u4E8E\u5B9A\u4E49\u5185\u5BB9\u7684\u7ED3\u6784\u548C\u8BED\u4E49\uFF0C"),
+  /* @__PURE__ */ createBaseVNode("code", null, "CSS"),
+  /* @__PURE__ */ createTextVNode("\u7528\u4E8E\u8BBE\u8BA1\u98CE\u683C\u548C\u5E03\u5C40\u3002\u6BD4\u5982\uFF0C\u60A8\u53EF\u4EE5\u4F7F\u7528"),
+  /* @__PURE__ */ createBaseVNode("code", null, "CSS"),
+  /* @__PURE__ */ createTextVNode("\u6765\u66F4\u6539\u5185\u5BB9\u7684\u5B57\u4F53\u3001\u989C\u8272\u3001\u5927\u5C0F\u3001\u95F4\u8DDD\uFF0C\u5C06\u5185\u5BB9\u5206\u4E3A\u591A\u5217\uFF0C\u6216\u8005\u6DFB\u52A0\u52A8\u753B\u53CA\u5176\u4ED6\u7684\u88C5\u9970\u6548\u679C\u3002")
+], -1);
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createElementBlock(Fragment, null, [
+    _hoisted_1,
+    _hoisted_2
+  ], 64);
+}
+var _01Css_html = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { _01Css_html as default };
---
+++ docs/.vuepress/dist/assets/01.html.2e7f9647.js
@@ -0,0 +1,147 @@
+import { r as resolveComponent, o as openBlock, c as createElementBlock, b as createBaseVNode, a as createVNode, F as Fragment, f as createStaticVNode, d as createTextVNode } from "./app.abab5812.js";
+import { _ as _export_sfc } from "./plugin-vue_export-helper.21dcd24c.js";
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode('<h1 id="flutter" tabindex="-1"><a class="header-anchor" href="#flutter" aria-hidden="true">#</a> Flutter</h1><ul><li><p>Kotlin \u662F\u4E00\u95E8\u7F16\u7A0B\u8BED\u8A00\uFF0C\u800C Flutter \u662F\u4E00\u4E2A\u8DE8\u5E73\u53F0\u7684 UI \u5DE5\u5177\u96C6\u3002</p></li><li><p>Flutter\u662F\u8C37\u6B4C\u7684\u79FB\u52A8UI\u6846\u67B6\uFF0C\u53EF\u4EE5\u5FEB\u901F\u5728iOS\u548CAndroid\u4E0A\u6784\u5EFA\u9AD8\u8D28\u91CF\u7684\u539F\u751F\u7528\u6237\u754C\u9762\uFF0C\u5F00\u6E90\u514D\u8D39\uFF0C\u5176\u4F7F\u7528\u4E86Dart\u8BED\u6CD5\uFF0C\u6240\u4EE5\u5B66\u4E60\u65F6\u4E5F\u9700\u8981\u5148\u4E86\u89E3\u4E00\u4E0BDart\uFF08\u5EFA\u8BAE\u800C\u5DF2\uFF09\u3002</p></li><li><p>Dart\u662F\u8C37\u6B4C\u5F00\u53D1\u7684\u8BA1\u7B97\u673A\u7F16\u7A0B\u8BED\u8A00\uFF0CDart\u662F\u9762\u5411\u5BF9\u8C61\u7684\u3001\u7C7B\u5B9A\u4E49\u7684\u3001\u5355\u7EE7\u627F\u7684\u8BED\u8A00\u3002\u5B83\u7684\u8BED\u6CD5\u7C7B\u4F3CC\u8BED\u8A00\uFF0C\u53EF\u4EE5\u8F6C\u8BD1\u4E3AJavaScript\u3002</p></li></ul><h2 id="\u73AF\u5883\u642D\u5EFA" tabindex="-1"><a class="header-anchor" href="#\u73AF\u5883\u642D\u5EFA" aria-hidden="true">#</a> \u73AF\u5883\u642D\u5EFA</h2><p>\u5148\u4E0B\u8F7D\u597D\u4E0B\u9762\u5DE5\u5177\uFF1A</p>', 4);
+const _hoisted_5 = {
+  href: "https://www.oracle.com/java/technologies/downloads/#java8",
+  target: "_blank",
+  rel: "noopener noreferrer"
+};
+const _hoisted_6 = /* @__PURE__ */ createTextVNode("JDK\u2014\u2014\uFF08\u4E00\u822Cjdk-8u251-windows-x64.exe\u5373\u53EF\uFF09");
+const _hoisted_7 = {
+  href: "https://developer.android.google.cn/studio?hl=zh-cn",
+  target: "_blank",
+  rel: "noopener noreferrer"
+};
+const _hoisted_8 = /* @__PURE__ */ createTextVNode("android-tool");
+const _hoisted_9 = /* @__PURE__ */ createTextVNode("\uFF0C\u8BE6\u7EC6\u6B65\u9AA4\u53EF\u4EE5\u770B\u540E\u9762\u5185\u5BB9\u3002");
+const _hoisted_10 = /* @__PURE__ */ createBaseVNode("p", null, "Flutter SDK\u4E0B\u8F7D\u5730\u5740\uFF1A", -1);
+const _hoisted_11 = /* @__PURE__ */ createTextVNode("\uFF081\uFF09");
+const _hoisted_12 = {
+  href: "https://flutter.dev/docs/get-started/install/windows#",
+  target: "_blank",
+  rel: "noopener noreferrer"
+};
+const _hoisted_13 = /* @__PURE__ */ createTextVNode("\u5B89\u88C5\u7248\u4E0B\u8F7D\u94FE\u63A5\u5730\u5740\u2014\u2014\u5BB9\u6613\u5D29");
+const _hoisted_14 = /* @__PURE__ */ createStaticVNode('<p>\uFF082\uFF09<code>github</code>\u4E0B\u8F7D\u547D\u4EE4-\u63A8\u8350</p><div class="language-git ext-git line-numbers-mode"><pre class="language-git"><code>git clone https://github.com/flutter/flutter.git\n</code></pre><div class="line-numbers"><span class="line-number">1</span><br></div></div><p>\u4E0B\u9762\u662F\u4E2D\u56FD\u5927\u9646\u7684\u955C\u50CF\uFF0C\u5EFA\u8BAE\u6DFB\u52A0\u5230\u7CFB\u7EDF\u73AF\u5883\u53D8\u91CF\u4E2D\uFF1A</p><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code><span class="token builtin class-name">export</span> <span class="token assign-left variable">PUB_HOSTED_URL</span><span class="token operator">=</span>https://pub.flutter-io.cn\n<span class="token builtin class-name">export</span> <span class="token assign-left variable">FLUTTER_STORAGE_BASE_URL</span><span class="token operator">=</span>https://storage.flutter-io.cn\n</code></pre><div class="line-numbers"><span class="line-number">1</span><br><span class="line-number">2</span><br></div></div>', 4);
+const _hoisted_18 = {
+  href: "https://www.yeshen.com/",
+  target: "_blank",
+  rel: "noopener noreferrer"
+};
+const _hoisted_19 = /* @__PURE__ */ createTextVNode("\u591C\u795E\u6A21\u62DF\u5668\u2014\u2014\u8DD1\u9879\u76EE\u7528");
+const _hoisted_20 = { class: "custom-container details" };
+const _hoisted_21 = /* @__PURE__ */ createBaseVNode("summary", null, "\u66F4\u591A", -1);
+const _hoisted_22 = /* @__PURE__ */ createBaseVNode("p", null, "Flutter\u96C6\u6210\u4E86Dart\uFF0C\u4E0B\u9762\u662F\u53EF\u9009\u7684\uFF1A", -1);
+const _hoisted_23 = {
+  href: "https://dart.cn/",
+  target: "_blank",
+  rel: "noopener noreferrer"
+};
+const _hoisted_24 = /* @__PURE__ */ createTextVNode("Dart\u5B98\u7F51");
+const _hoisted_25 = {
+  href: "https://gekorm.com/dart-windows/",
+  target: "_blank",
+  rel: "noopener noreferrer"
+};
+const _hoisted_26 = /* @__PURE__ */ createTextVNode("Dart\u5B89\u88C5\u5305\u4E0B\u8F7D");
+const _hoisted_27 = /* @__PURE__ */ createStaticVNode('<h2 id="jdk\u5B89\u88C5" tabindex="-1"><a class="header-anchor" href="#jdk\u5B89\u88C5" aria-hidden="true">#</a> JDK\u5B89\u88C5</h2><ol><li>\u4E0B\u8F7D\u540E\u9ED8\u8BA4\u5B89\u88C5</li><li>\u914D\u7F6E\u73AF\u5883\u53D8\u91CF\uFF1A\u6211\u7684\u7535\u8111\u53F3\u51FB-&gt;\u5C5E\u6027-&gt;\u9AD8\u7EA7\u7CFB\u7EDF\u8BBE\u7F6E-&gt;\u73AF\u5883\u53D8\u91CF</li><li>\u65B0\u5EFAJAVA_HOME\uFF1A C:\\Program Files\\Java\\jdk1.8.0_191</li><li>\u65B0\u5EFACLASSPATH\uFF1A .;%JAVA_HOME%\\lib;%JAVA_HOME%\\lib\\tools.jar</li><li>\u7F16\u8F91Path\u672B\u5C3E\u589E\u52A0\uFF1A %JAVA_HOME%\\bin;%JAVA_HOME%\\jre\\bin</li><li>\u6700\u540E\u68C0\u67E5\u914D\u7F6E\u662F\u5426\u6B63\u786E\uFF1A \u8C03\u51FA\u7CFB\u7EDFcmd\u547D\u4EE4\u7A97\u53E3\u8F93\u5165<code>java -version</code>\u7B49\u547D\u4EE4</li></ol><h2 id="\u5B89\u5353\u73AF\u5883" tabindex="-1"><a class="header-anchor" href="#\u5B89\u5353\u73AF\u5883" aria-hidden="true">#</a> \u5B89\u5353\u73AF\u5883</h2>', 3);
+const _hoisted_30 = {
+  href: "https://developer.android.google.cn/studio?hl=zh-cn",
+  target: "_blank",
+  rel: "noopener noreferrer"
+};
+const _hoisted_31 = /* @__PURE__ */ createTextVNode("android-tool");
+const _hoisted_32 = /* @__PURE__ */ createStaticVNode('<ol><li><p>\u6253\u5F00\u7F51\u9875\u540E\uFF0C\u4E0D\u7528\u7BA1Android Studio\u76F8\u5173\u7684\u4E0B\u8F7D\uFF0C\u76F4\u63A5\u5F80\u4E0B\u62C9\uFF0C\u62C9\u5230\u6700\u5E95\u4E0B\u3002\u7136\u540E\u518D\u5411\u4E0A\u7A0D\u5FAE\u6EDA\u52A8\u51E0\u4E0B\uFF0C\u770B\u5230\u5982\u4E0B\u754C\u9762\uFF0C\u9009\u62E9\u76F8\u5E94\u7684\u7CFB\u7EDF\u4E0B\u8F7D\u5373\u53EF</p></li><li><p>\u63A5\u7740\u89E3\u538B\u4E0B\u8F7D\u7684\u538B\u7F29\u6587\u4EF6\uFF0C\u5E76\u628A\u91CC\u9762\u7684tools\u89E3\u538B\u5230\u5B89\u5353\u8F6F\u4EF6\u76F8\u5173\u7684\u76EE\u5F55\u4E0B\uFF0C\u6211\u8FD9\u8FB9\u662F<code>E:\\Java\\android\\sdk</code></p></li><li><p>\u628A\u89E3\u538B\u4E4B\u540E\u7684\u76EE\u5F55\u6DFB\u52A0\u5230\u73AF\u5883\u53D8\u91CFpath\u4E2D\uFF0C\u6DFB\u52A0:</p></li></ol><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code>E:<span class="token punctuation">\\</span>Java<span class="token punctuation">\\</span>android<span class="token punctuation">\\</span>sdk<span class="token punctuation">\\</span>tools\nE:<span class="token punctuation">\\</span>Java<span class="token punctuation">\\</span>android<span class="token punctuation">\\</span>sdk<span class="token punctuation">\\</span>tools<span class="token punctuation">\\</span>bin\n\n<span class="token comment"># \u4E0D\u8981\u91CD\u590D\u6DFB\u52A0</span>\n</code></pre><div class="line-numbers"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br></div></div><ol start="4"><li>\u6253\u5F00\u7EC8\u7AEF(\u5EFA\u8BAE\u7BA1\u7406\u5458)\uFF0C\u952E\u5165<code>sdkmanager --list</code>\uFF0C\u5176\u4E2D\u7684Installed packages\u662F\u5DF2\u7ECF\u5B89\u88C5\u7684\u5305\u5217\u8868\uFF0C\u800CAvailable Packages\u662F\u672A\u5B89\u88C5\u7684\u5305\u5217\u8868\u3002\u5B89\u88C5\u9700\u8981\u7684\u5373\u53EF\u3002</li></ol><p>\u6CE8\u610F\uFF1A\u5982\u679C\u952E\u5165<code>sdkmanager --list</code>\u62A5\u9519\uFF1B\u770B\u4E00\u4E0B\u9519\u8BEF\u4FE1\u606F\uFF0C\u56E0\u4E3AAS\u89E3\u538B\u53EF\u80FD\u662F\u8DEF\u5F84\u9519\u8BEF\uFF0C\u5982SDK\u89E3\u538B\u540E\u6587\u4EF6\u5939\u4E3A<code>cmdline-tools</code>\uFF0C\u4E0D\u8981\u6539\u52A8\uFF0C\u76F4\u63A5\u89E3\u538B\u5230\u4F60\u7684\u76EE\u5F55\uFF0C<code>cmdline-tools</code>\u5EFA\u7ACB<code>latest</code>\u6587\u4EF6\u5939\uFF0C\u628A\u5185\u5BB9\u5168\u90E8\u653E\u8FDB\u53BB\u3002\u8BD5\u8BD5\uFF0C\u5176\u5B83\u9519\u8BEF\u7F51\u4E0A\u4E00\u822C\u6709\u4EBA\u5206\u4EAB\u89E3\u51B3\u529E\u6CD5\u3002</p><ol start="5"><li>\u547D\u4EE4\u884C\u5DE5\u5177\u4E0B\u8F7D\u57FA\u7840\u5DE5\u5177\u5305</li></ol><p>\u4F7F\u7528sdkmanager --list\u8F93\u51FA\u53EF\u4EE5\u5B89\u88C5\u7684\u5305;\u6211\u4EEC\u5B89\u88C5\u7684\u6709\uFF1A</p><ul><li>cmdline-tools:latest</li><li>platform-tools</li></ul><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code>sdkmanager --install cmdline-tools<span class="token punctuation">;</span>latest platform-tools\n</code></pre><div class="line-numbers"><span class="line-number">1</span><br></div></div><p>\u6DFB\u52A0\u73AF\u5883\u53D8\u91CF<code>E:\\Java\\android\\sdk\\cmdline-tools\\latest\\bin</code>\u548C<code>E:\\Java\\android\\sdk\\platform-tools</code>\u5230<code>path</code></p><ol start="6"><li>\u547D\u4EE4\u884C\u5DE5\u5177\u4E0B\u8F7D\u5B89\u5353SDK</li></ol>', 10);
+const _hoisted_42 = /* @__PURE__ */ createTextVNode("\u786E\u5B9A\u6211\u4EEC\u9700\u8981\u5F00\u53D1\u7684api\u7EA7\u522B\uFF0C\u4E0D\u540C\u7684api\u5BF9\u5E94\u4E0D\u540C\u7684\u7CFB\u7EDF\u7248\u672C\uFF0C\u5177\u4F53\u53EF\u4EE5");
+const _hoisted_43 = {
+  href: "https://developer.android.google.cn/guide/topics/manifest/uses-sdk-element?hl=zh-cn",
+  target: "_blank",
+  rel: "noopener noreferrer"
+};
+const _hoisted_44 = /* @__PURE__ */ createTextVNode("\u53C2\u8003");
+const _hoisted_45 = /* @__PURE__ */ createTextVNode("\u6211\u4EEC\u8FD9\u91CC\u7528API \u7EA7\u522B\u4E3A28\u7684sdk");
+const _hoisted_46 = /* @__PURE__ */ createStaticVNode('<p>\u952E\u5165<code>sdkmanager --list</code>:</p><ul><li><code>build-tools 28.0.3</code>\u662F\u7F16\u8BD1\u65F6\u5BF9\u5E94\u7684\u7F16\u8BD1\u5DE5\u5177</li><li><code>platforms android-28</code>\u4E3B\u8981\u662F\u7CFB\u7EDF\u7684jar\u5305</li></ul><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code>sdkmanager --install build-tools<span class="token punctuation">;</span><span class="token number">28.0</span>.3 platforms<span class="token punctuation">;</span>android-28\n</code></pre><div class="line-numbers"><span class="line-number">1</span><br></div></div><ol start="7"><li>\u4E0B\u8F7D\u4EFF\u771F\u955C\u50CF\u53CA\u6A21\u62DF\u5668</li></ol><p>\u8FD9\u4E00\u6B65\u975E\u5FC5\u987B\uFF0C\u5982\u679C\u4F60\u7528\u771F\u673A\u8C03\u8BD5\u3002\u8FD8\u662F\u7EE7\u7EED\u5427\uFF1A</p><ul><li><code>emulator</code>\u662F\u6A21\u62DF\u5668</li><li><code>system-images;android-28;google_apis_playstore;x86_64</code>\u5219\u662F\u7CFB\u7EDF\u7684\u955C\u50CF\u6587\u4EF6\uFF0C\u53EF\u968F\u610F</li></ul><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code>sdkmanager --install emulator system-images<span class="token punctuation">;</span>android-28<span class="token punctuation">;</span>google_apis_playstore<span class="token punctuation">;</span>x86_64\n</code></pre><div class="line-numbers"><span class="line-number">1</span><br></div></div><ol start="8"><li>\u8BBE\u7F6E<code>GRADLE_USER_HOME</code></li></ol><p>\u8FD9\u6B65\u53EF\u9009\uFF0C\u56E0\u4E3A\u7CFB\u7EDF\u4F1A\u9ED8\u8BA4\u5728\u7528\u6237\u76EE\u5F55\u4E0B\u521B\u5EFA\u4E00\u4E2A.gradle\u6587\u4EF6\u5939\u7531\u4E8E\u5B58\u653Egradle\u7684\u5404\u4E2A\u7248\u672C\uFF1B</p><p>\u7F3A\u70B9\uFF1A</p><ul><li>1\u3001\u5207\u6362\u7528\u6237\u65F6\u9700\u8981\u91CD\u65B0\u4E0B\u8F7D\u66F4\u65B0\u3002</li><li>2\u3001\u65F6\u95F4\u957F\u4E86\uFF0C\u7248\u672C\u591A\u4E86\u4F1A\u5360\u7528c\u76D8\u5B58\u50A8\uFF0C\u6240\u4EE5\u6211\u8BBE\u7F6E\u4E86\u73AF\u5883\u53D8\u91CF<code>GRADLE_USER_HOME</code>\uFF0C\u5E76\u8D4B\u503C\u4E3A<code>E:\\android\\gradlepro</code>\u3002</li></ul><h2 id="flutter\u5B89\u88C5" tabindex="-1"><a class="header-anchor" href="#flutter\u5B89\u88C5" aria-hidden="true">#</a> Flutter\u5B89\u88C5</h2><p>\u5C06\u4F60\u51C6\u5907\u597D\u7684\u5B89\u88C5\u5305\u89E3\u538B\u5230\u4F60\u60F3\u5B89\u88C5Flutter SDK\u7684\u8DEF\u5F84\uFF0C\u4E0D\u8981\u9AD8\u5371\u8DEF\u5F84\uFF0C\u5982:<code>C:\\Program Files\\</code></p><ul><li>\u7F16\u8F91<code>Path</code>\u589E\u52A0\uFF1A <code>C:\\Flutter\\flutter\\bin</code> \u6B64\u4E3A\u4F60\u89E3\u538B\u7684\u76EE\u5F55</li></ul><p>\u5728cmd\u4F7F\u7528<code>flutter doctor</code>\u547D\u4EE4\u884C\u68C0\u6D4B</p><p>\u56E0\u4E3A\u6CA1\u6709AS\uFF0C\u6240\u4EE5\u6709\u62A5\u9519\u6CA1\u5173\u7CFB\uFF0C\u6211\u4EEC\u9700\u8981\u7684\u6CA1\u9519\u5C31\u884C\u3002</p><h2 id="vscode\u914D\u7F6E" tabindex="-1"><a class="header-anchor" href="#vscode\u914D\u7F6E" aria-hidden="true">#</a> VSCode\u914D\u7F6E</h2>', 17);
+const _hoisted_63 = {
+  href: "https://code.visualstudio.com/",
+  target: "_blank",
+  rel: "noopener noreferrer"
+};
+const _hoisted_64 = /* @__PURE__ */ createTextVNode("VSCode");
+const _hoisted_65 = /* @__PURE__ */ createStaticVNode('<p>\u5B89\u88C5\u4E3B\u8981\u63D2\u4EF6\uFF1A</p><p><code>dart</code><code>flutter</code></p><p>\u53E6\u5916\u4E00\u4E9B\u53EF\u9009\u7684\u63D2\u4EF6\uFF1A</p><h2 id="\u7B2C\u4E00\u4E2A\u5E94\u7528" tabindex="-1"><a class="header-anchor" href="#\u7B2C\u4E00\u4E2A\u5E94\u7528" aria-hidden="true">#</a> \u7B2C\u4E00\u4E2A\u5E94\u7528</h2><p>\u6253\u5F00VSCode</p><ol start="4"><li><p>\u6307\u5B9A\u653E\u7F6E\u9879\u76EE\u7684\u4F4D\u7F6E\u5E76\u786E\u5B9A</p></li><li><p>\u7B49\u5F85\u9879\u76EE\u4E0B\u8F7D\u597D\u6240\u6709\u8D44\u6E90\u540E\u4F1A\u663E\u793Amain.dart\u6587\u4EF6\u5C31ok\u4E86\u3002</p></li><li><p>\u6700\u540E\u5728vscode\u7684\u53F3\u4E0B\u89D2\u70B9\u51FBNo Devices\u9009\u62E9\u6A21\u62DF\u5668\uFF0C\u6309F5\u8FD0\u884C\u3002</p></li><li><p>\u6B64\u65F6\u4F60\u53EF\u4EE5\u8FD0\u884C\u5728\u6D4F\u89C8\u5668\u4E2D\uFF0C\u56E0\u4E3A\u5927\u90E8\u4EBA\u521B\u5EFADevices\u65E0\u6CD5\u542F\u52A8\u3002</p></li></ol><h2 id="\u591C\u795E\u865A\u62DF\u673A" tabindex="-1"><a class="header-anchor" href="#\u591C\u795E\u865A\u62DF\u673A" aria-hidden="true">#</a> \u591C\u795E\u865A\u62DF\u673A</h2><p>\u5728VSCode\u9009\u62E9\u542F\u52A8\u9879\u91CC\u5982\u679C\u627E\u4E0D\u5230\u591C\u795E\u865A\u62DF\u673A\uFF0C\u53EF\u80FD\u662F\u4EE5\u4E0B\u60C5\u51B5\uFF1A</p><ol><li><p>Win10\u6253\u5F00\u591C\u795E\u6A21\u62DF\u5668\u7684bin\u76EE\u5F55,\u8FDB\u5165cmd\u952E\u5165\uFF1A<code>nox_adb.exe connect 127.0.0.1:62001</code>;\u67E5\u770B\u662F\u5426\u542F\u52A8\u6210\u529F\u3002</p></li><li><p>PC\u7AEF\u7684adb\u6587\u4EF6\u6709\u4E24\u4E2A\u4E0D\u540C\u7684\u7248\u672C\u3002</p></li></ol><p>cmd\u8FDB\u5165\u6253\u5F00<code>android SDK</code>\u7684<code>platform-tools</code>\u76EE\u5F55\u4E0B\uFF1A</p><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code>adb version\n</code></pre><div class="line-numbers"><span class="line-number">1</span><br></div></div><p>cmd\u8FDB\u5165\u591C\u795E\u6A21\u62DF\u5668\u7684bin\u76EE\u5F55\u4E0B\uFF1A</p><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code>adb version\n<span class="token comment"># \u7248\u672C\u4E0Eandroid SDKb\u4E0D\u540C</span>\n</code></pre><div class="line-numbers"><span class="line-number">1</span><br><span class="line-number">2</span><br></div></div><p>\u6253\u5F00<code>android SDK</code>\u7684<code>platform-tools</code>\u76EE\u5F55\u4E0B\u7684<code>adb.exe</code>\u62F7\u8D1D\u5230\u591C\u795E\u6A21\u62DF\u5668\u7684bin\u76EE\u5F55\u4E0B\uFF0C\u76F4\u63A5\u8986\u76D6\uFF0C\u5E76\u4E14\u518D\u590D\u5236\u4E00\u4EFD\uFF0C\u91CD\u547D\u540D\u4E3A<code>nox_adb.exe</code>\uFF0C\u91CD\u542F\u8BD5\u8BD5\u770B\u3002\uFF08\u6700\u597D\u5C06\u591C\u795E\u7684<code>adb.exe</code>\u5907\u4EFD\u4E00\u4E0B\uFF0C\u968F\u610F\uFF09</p>', 14);
+function _sfc_render(_ctx, _cache) {
+  const _component_ExternalLinkIcon = resolveComponent("ExternalLinkIcon");
+  return openBlock(), createElementBlock(Fragment, null, [
+    _hoisted_1,
+    createBaseVNode("p", null, [
+      createBaseVNode("a", _hoisted_5, [
+        _hoisted_6,
+        createVNode(_component_ExternalLinkIcon)
+      ])
+    ]),
+    createBaseVNode("p", null, [
+      createBaseVNode("a", _hoisted_7, [
+        _hoisted_8,
+        createVNode(_component_ExternalLinkIcon)
+      ]),
+      _hoisted_9
+    ]),
+    _hoisted_10,
+    createBaseVNode("p", null, [
+      _hoisted_11,
+      createBaseVNode("a", _hoisted_12, [
+        _hoisted_13,
+        createVNode(_component_ExternalLinkIcon)
+      ])
+    ]),
+    _hoisted_14,
+    createBaseVNode("p", null, [
+      createBaseVNode("a", _hoisted_18, [
+        _hoisted_19,
+        createVNode(_component_ExternalLinkIcon)
+      ])
+    ]),
+    createBaseVNode("details", _hoisted_20, [
+      _hoisted_21,
+      _hoisted_22,
+      createBaseVNode("p", null, [
+        createBaseVNode("a", _hoisted_23, [
+          _hoisted_24,
+          createVNode(_component_ExternalLinkIcon)
+        ])
+      ]),
+      createBaseVNode("p", null, [
+        createBaseVNode("a", _hoisted_25, [
+          _hoisted_26,
+          createVNode(_component_ExternalLinkIcon)
+        ])
+      ])
+    ]),
+    _hoisted_27,
+    createBaseVNode("p", null, [
+      createBaseVNode("a", _hoisted_30, [
+        _hoisted_31,
+        createVNode(_component_ExternalLinkIcon)
+      ])
+    ]),
+    _hoisted_32,
+    createBaseVNode("p", null, [
+      _hoisted_42,
+      createBaseVNode("a", _hoisted_43, [
+        _hoisted_44,
+        createVNode(_component_ExternalLinkIcon)
+      ]),
+      _hoisted_45
+    ]),
+    _hoisted_46,
+    createBaseVNode("p", null, [
+      createBaseVNode("a", _hoisted_63, [
+        _hoisted_64,
+        createVNode(_component_ExternalLinkIcon)
+      ])
+    ]),
+    _hoisted_65
+  ], 64);
+}
+var _01_html = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { _01_html as default };
