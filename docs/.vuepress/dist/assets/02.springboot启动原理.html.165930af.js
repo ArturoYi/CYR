44e34a2145f4913f0b9f7eeae085bc1e267f2bd0
IU_UI <3458269236@qq.com>
2022-08-16 18:46:05 +0800

    2022-08-16更新

---
+++ docs/.vuepress/dist/assets/02.springboot启动原理.html.165930af.js
@@ -0,0 +1,56 @@
+const data = {
+  "key": "v-1f229241",
+  "path": "/java/springboot/02.springboot%E5%90%AF%E5%8A%A8%E5%8E%9F%E7%90%86.html",
+  "title": "springboot\u542F\u52A8\u539F\u7406",
+  "lang": "zh-CN",
+  "frontmatter": {
+    "lang": "zh-CN",
+    "title": "springboot\u542F\u52A8\u539F\u7406",
+    "description": "springboot"
+  },
+  "excerpt": "",
+  "headers": [
+    {
+      "level": 2,
+      "title": "\u4E3B\u7C7B\u4EE3\u7801\u5F00\u59CB\uFF08\u6CE8\u89E3\u4E5F\u4F9D\u8D56\u4E8E\u4EE3\u7801\uFF09",
+      "slug": "\u4E3B\u7C7B\u4EE3\u7801\u5F00\u59CB-\u6CE8\u89E3\u4E5F\u4F9D\u8D56\u4E8E\u4EE3\u7801",
+      "children": []
+    },
+    {
+      "level": 2,
+      "title": "\u76F4\u63A5new\u4E86\u4E00\u4E2Aspring boot\u5BF9\u8C61",
+      "slug": "\u76F4\u63A5new\u4E86\u4E00\u4E2Aspring-boot\u5BF9\u8C61",
+      "children": []
+    },
+    {
+      "level": 2,
+      "title": "getSpringFactoriesInstances",
+      "slug": "getspringfactoriesinstances",
+      "children": []
+    },
+    {
+      "level": 2,
+      "title": "\u603B\u7ED3run\u65B9\u6CD5",
+      "slug": "\u603B\u7ED3run\u65B9\u6CD5",
+      "children": []
+    },
+    {
+      "level": 2,
+      "title": "\u81EA\u52A8\u914D\u7F6E\u539F\u7406",
+      "slug": "\u81EA\u52A8\u914D\u7F6E\u539F\u7406",
+      "children": []
+    }
+  ],
+  "git": {
+    "updatedTime": 1660646703e3,
+    "contributors": [
+      {
+        "name": "IU_UI",
+        "email": "3458269236@qq.com",
+        "commits": 1
+      }
+    ]
+  },
+  "filePathRelative": "java/springboot/02.springboot\u542F\u52A8\u539F\u7406.md"
+};
+export { data };
