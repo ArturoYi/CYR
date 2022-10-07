3e80ce3fe5967fad9fbd7b6d0b111f267c8f14d0
陈柒 <3062995371@qq.com>
2022-10-07 18:47:29 +0800

    2022-10-07更新

---
+++ docs/.vuepress/dist/assets/02.组件编写.html.b833ad71.js
@@ -0,0 +1,62 @@
+const data = {
+  "key": "v-af7ef9fc",
+  "path": "/web/vue3/02.%E7%BB%84%E4%BB%B6%E7%BC%96%E5%86%99.html",
+  "title": "Vue3\u7EC4\u4EF6\u7BC7",
+  "lang": "zh-CN",
+  "frontmatter": {
+    "lang": "zh-CN",
+    "title": "Vue3\u7EC4\u4EF6\u7BC7",
+    "description": "vue\u63CF\u8FF0"
+  },
+  "excerpt": "",
+  "headers": [
+    {
+      "level": 2,
+      "title": "setup",
+      "slug": "setup",
+      "children": []
+    },
+    {
+      "level": 2,
+      "title": "ref",
+      "slug": "ref",
+      "children": []
+    },
+    {
+      "level": 2,
+      "title": "reactive",
+      "slug": "reactive",
+      "children": []
+    },
+    {
+      "level": 2,
+      "title": "toRef \u5168\u5BB6\u6876",
+      "slug": "toref-\u5168\u5BB6\u6876",
+      "children": []
+    },
+    {
+      "level": 2,
+      "title": "computed",
+      "slug": "computed",
+      "children": []
+    },
+    {
+      "level": 2,
+      "title": "watch",
+      "slug": "watch",
+      "children": []
+    }
+  ],
+  "git": {
+    "updatedTime": 1665128331e3,
+    "contributors": [
+      {
+        "name": "\u9648\u67D2",
+        "email": "3062995371@qq.com",
+        "commits": 2
+      }
+    ]
+  },
+  "filePathRelative": "web/vue3/02.\u7EC4\u4EF6\u7F16\u5199.md"
+};
+export { data };
